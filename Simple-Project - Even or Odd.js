fff4d9ea819cbca3ff60507a94864b6784cdde48
JimmyFly90 <137488857+JimmyFly90@users.noreply.github.com>
2024-01-17 16:15:53 +0000

    Add files via upload

--- Simple-Project - Even or Odd.js
+++ Simple-Project - Even or Odd.js
@@ -1,14 +1,23 @@
+// create function to show if number is even or odd
+// link input number to work with the Go button
+// when number and Go button pressed, show Even or Odd
+
 function even_or_odd(number) {
     if (number % 2) {
         return ('Odd');
-    } else {
-        return ('Even');
+    } else { 
+        return ('Even');   
     }
 }
 ;
+
+function generate() {
+    let x = document.getElementById("typedNumber").value;
+    document.getElementById("answer").innerHTML = (even_or_odd(x));
+};
 
 console.log(even_or_odd(0)); 
 console.log(even_or_odd(2)); 
 console.log(even_or_odd(3)); 
 console.log(even_or_odd(-3)); 
-console.log(even_or_odd(24));
+console.log(even_or_odd("trifle"));
